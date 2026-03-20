30233a920c3e6d97125bd3070cf6d39a07bc581b
Benjamin <benjaminaravena999@gmail.com>
2026-03-19 22:13:05 -0300

    next.config.js update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // La URL del backend. En producción, configura NEXT_PUBLIC_API_URL en tu host.
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  output: 'standalone',
 };
 module.exports = nextConfig;
